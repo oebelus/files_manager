1991b260e8e552816001e5f9d5c1d94e6b6092ca
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-03-20 16:06:56 +0000

    update utils functions

--- utils.py
+++ utils.py
@@ -13,20 +13,36 @@ def get_extension(file):
 def get_directories(folder_path):
     return [folder for folder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, folder))]
 
-def display_directories(directory_array, isPdf):
+def display_directories(directory_array):
     count = 1
     for directory in directory_array:
         print(f'({count})> {directory}')
         count += 1
+    print('Enter (h) to view the commands')
+
+def commands_list():
+    print("<-------------------------------------->")
     print('> Enter a number to access the directory')
-    print('> Enter (r) to read the file') if isPdf else None
+    print('> Enter (r) to read the file')
     print('> Enter (.) to save the file here')
+    print('> Enter (..) to navigate to the parent directory')
     print('> Enter (+) to create a directory')
-    print('> Enter (-) to cancel')
+    print('> Enter (d) to delete the file')
+    print('> Enter (D) to delete a directory')
+    print('> Enter (n) to show the name of the file')
+    print('> Enter (re) to rename the file')
+    print('> Enter (dirs) to display directories')
+    print('> Enter (redir) to rename a directory')
+    print('> Enter (mv) to move a directory')
+    print('> Enter (-) to move to the next file')
+    print('> Enter (q) to exit')
 
 def read_file(file_path):
     with open(file_path) as file:
-        print(file.readlines())
+        try:
+            print(file.readlines())
+        except:
+            print("Error reading file")
 
 def check_pdf(file_path):
     return get_extension(file_path) == '.pdf'
@@ -41,3 +57,12 @@ def read_pdf(pdf_path, page_number):
     else :
         page = pdf.pages[abs(page_number)-1]
     return page.extract_text()
+
+def get_parent_directory(directory):
+    count = 1
+    indexes = []
+    for char in directory:
+        if char == '\\':
+            indexes.append(count)
+        count += 1
+    return directory[0:indexes[-1]-1]
